4d98d992785712268635ae1a238ef672678c4d73
Vinicius Alves <103587422+Viniciusalves16@users.noreply.github.com>
2025-06-05 18:16:13 -0300

    Iniciando projeto ✅

---
+++ src/test/java/com/example/NotificationDispatcher/NotificationDispatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NotificationDispatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationDispatcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
